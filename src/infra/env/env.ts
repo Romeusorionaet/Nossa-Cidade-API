0f6ec0377384d0c565bc2d105b6b7ea7a03ac000
Romeu Sorionaet <romeusoares14569@gmail.com>
2025-01-28 14:46:49 -0300

    chore: install new dependencies and updated env schema

--- src/infra/env/env.ts
+++ src/infra/env/env.ts
@@ -4,6 +4,9 @@ export const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
   PORT: z.coerce.number().optional().default(3333),
   DATABASE_URL: z.string(),
+  NOSSA_CIDADE_HOST: z.string(),
+  JWT_PUBLIC_KEY: z.string(),
+  JWT_PRIVATE_KEY: z.string(),
 });
 
 export type Env = z.infer<typeof envSchema>;
